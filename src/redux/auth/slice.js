7d117940f2aad141ce54523ac31bfef09d869803
Yulia Kutova <kutova85@gmail.com>
2023-04-24 22:46:41 +0300

    redux

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,112 @@
+// import { createSlice } from '@reduxjs/toolkit';
+// import { register, logIn, logOut, refreshUser } from './operations';
+
+// const initialState = {
+//   user: { name: null, email: null },
+//   token: null,
+//   isLoggedIn: false,
+//   isRefreshing: false,
+// };
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   extraReducers: builder =>
+//     builder
+//       .addCase(register.fulfilled, (state, action) => {
+//         state.user = action.payload.user;
+//         state.token = action.payload.token;
+//         state.isLoggedIn = true;
+//       })
+//       .addCase(logIn.fulfilled, (state, action) => {
+//         state.user = action.payload.user;
+//         state.token = action.payload.token;
+//         state.isLoggedIn = true;
+//       })
+//       .addCase(logOut.fulfilled, (state, action) => {
+//         state.user = { name: null, email: null };
+//         state.token = null;
+//         state.isLoggedIn = false;
+//       })
+//       .addCase(refreshUser.pending, state => {
+//         state.isRefreshing = true;
+//       })
+//       .addCase(refreshUser.fulfilled, (state, action) => {
+//         state.user = action.payload;
+//         state.isLoggedIn = true;
+//         state.isRefreshing = false;
+//       })
+//       .addCase(refreshUser.rejected, state => {
+//         state.isRefreshing = false;
+//       }),
+// });
+
+// export const authReducer = authSlice.reducer;
+
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { register, logIn, logOut, refreshUser } from './operations';
+
+const authInitialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  isLoading: false,
+  error: null,
+};
+
+const extraActions = [register, logIn, logOut];
+
+const handleSendAuth = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+  state.isLoading = false;
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: authInitialState,
+  extraReducers: builder =>
+    builder
+      .addCase(register.fulfilled, handleSendAuth)
+      .addCase(logIn.fulfilled, handleSendAuth)
+      .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(refreshUser.pending, state => {
+        state.isRefreshing = true;
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, state => {
+        state.isRefreshing = false;
+        state.isLoading = false;
+      })
+      .addMatcher(
+        isAnyOf(...extraActions.map(action => action.pending)),
+        state => {
+          state.isLoading = true;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(...extraActions.map(action => action.rejected)),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+        }
+      ),
+});
+
+export const authReducer = authSlice.reducer;
