5ea9b35558d61d0d1f5fb6379e916bd9b62efcf8
Yulia Kutova <kutova85@gmail.com>
2023-04-24 22:46:16 +0300

    add pages

---
+++ src/pages/Contacts.js
@@ -0,0 +1,30 @@
+// import { GlobalStyle } from './GlobalStyle';
+import { ContactForm } from '../components/ContactForm/ContactForm';
+import { ContactList } from '../components/ContactList/ContactList';
+import { Filter } from '../components/Filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+import { Loader } from '../components/Loader/Loader';
+
+export const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && !error && <Loader />}
+      <ContactList />
+      {/* <GlobalStyle /> */}
+    </div>
+  );
+};
