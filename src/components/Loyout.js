7d27c82a2f05cf564bbab8ee3ad7fac23f9ca4d3
Yulia Kutova <kutova85@gmail.com>
2023-04-24 22:43:28 +0300

    add Navigation

---
+++ src/components/Loyout.js
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AppBar } from './AppBar/AppBar';
+import { Suspense } from 'react';
+import { LayoutStyled } from './Layout.styled';
+import { GlobalStyle } from './GlobalStyle';
+
+export const Layout = () => {
+  return (
+    <LayoutStyled>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+      <Toaster position="top-right" reverseOrder={false} />
+      <GlobalStyle />
+    </LayoutStyled>
+  );
+};
