c874faefc54d460b5a9fc56818bf69be1580b0eb
Yulia Kutova <kutova85@gmail.com>
2023-04-24 22:42:45 +0300

    add

---
+++ src/components/Layout.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+export const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  color: white;
+  border: 3px solid #ac40e5;
+  border-radius: 5px;
+  background: linear-gradient(to right, #03001e, #7303c0, #ec38bc, #fdeff9);
+  /* align-items: center; */
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
